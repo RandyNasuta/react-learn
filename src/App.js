bea03bc50ed5d78766c4d179512fbaa903612e34
randy.nasuta <randy.nasuta@binus.ac.id>
2023-06-21 10:40:25 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import "./App.css";
 function App() {
   const title = "welcome to the new blog";
   const likes = 50;
+  const link = "https://www.youtube.com/@AmaneKanata";
 
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
         <p>Liked {likes}</p>
+        <p>{10}</p>
+        <p>{"hello, ninjas"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+        <a href={link} target="blank">
+          Kanata
+        </a>
       </div>
     </div>
   );
